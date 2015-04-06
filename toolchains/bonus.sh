ee7c803b35a56f2f2aca115567bf5cd2d4935eaa
Hervé Boutemy <hboutemy@apache.org>
2015-04-06 23:34:11 +0200

    bonus plus fluide

--- toolchains/bonus.sh
+++ toolchains/bonus.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 clear
 
 run() {
@@ -6,20 +7,33 @@ run() {
   $*
 }
 
-readline() {
+commentaire() {
   echo ""
   echo -e "\\033[32m// $*\\033[0m"
+}
+
+enter() {
+  echo ""
+  echo -e "\\033[31m[...]\\033[0m"
   read
 }
 
-readline "démo bonus cas étonnant : la compilation avec un JDK 8 crée une dépendance à l'API JDK 8..."
+commentaire "cas étonnant : la compilation avec un JDK 8, du fait d'optimisation aggressives, crée une dépendance à l'API JDK 8..."
 j8
 run mvn -V clean package exec:exec javadoc:javadoc
-readline "Animal Sniffer détecte le problème (même s'il plante lamentablement avec un NPE)..."
+commentaire "Animal Sniffer détecte le problème (même s'il plante lamentablement avec un NPE)..."
+
+enter
+
+commentaire "L'exécution avec un JRE 7 montre plus clairement le problème (qui ne provient pas du source)..."
 j7
 run java -cp target/classes toolchains.Main
-readline "le problème est bien détecté par Animal Sniffer, mais cela ne le résoud pas: seul toolchain le peut..."
 
+commentaire "le problème est bien détecté par Animal Sniffer, mais cela ne le résoud pas."
+commentaire "il ne faut pas utiliser un JDK 8 pour lancer Maven, ou bien utiliser Toolchains..."
 
-readline "autre bonus : on peut exécuter jdeps du JDK 8/9 alors que Maven utilise JDK 7 (nécessite Maven 3.3)..."
+enter
+
+clear
+commentaire "bonus : alors que Maven utilise JDK 7, on peut exécuter jdeps du JDK 8/9 (nécessite Maven 3.3)..."
 run mvn -V clean package exec:exec javadoc:javadoc -Pjdeps
